044578120f4c2d6d79417c69a038c54eebf77acf
SubjectCarter <85772471+SubjectCarter@users.noreply.github.com>
2021-08-28 20:05:47 -0400

    EndTut9

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -1,10 +1,11 @@
-from django.db.models.query import QuerySet
+from django.http.response import JsonResponse
 from django.shortcuts import render
-from rest_framework import generics, request, serializers, status
+from rest_framework import generics, status
 from .serializers import RoomSerializer, CreateRoomSerializer
 from .models import Room
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.http import JsonResponse
 
 # Create your views here.
 
@@ -14,15 +15,50 @@ class RoomView(generics.ListAPIView):
     serializer_class = RoomSerializer
 
 
-class CreateRoomView(APIView):
-    serializer_class = CreateRoomSerializer
+class GetRoom(APIView):
+    serializer_class = RoomSerializer
+    lookup_url_kwarg = 'code'
 
-    def post(self, requst, format=None):
+    def get(self, request, format=None):
+        code = request.GET.get(self.lookup_url_kwarg)
+        if code != None:
+            room = Room.objects.filter(code=code)
+            if len(room) > 0:
+                data = RoomSerializer(room[0]).data
+                data['is_host'] = self.request.session.session_key == room[0].host
+                return Response(data, status=status.HTTP_200_OK)
+            return Response({'Room Not Found': 'Invalid Room Code.'}, status=status.HTTP_404_NOT_FOUND)
+
+        return Response({'Bad Request': 'Code paramater not found in request'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class JoinRoom(APIView):
+    lookup_url_kwarg = 'code'
+
+    def post(self, request, format=None):
         if not self.request.session.exists(self.request.session.session_key):
             self.request.session.create()
 
-        #serializer = self.serializer_class(data=request.data) Orginal Tut Line
-        serializer = self.serializer_class(data=self.request.data)
+        code = request.data.get(self.lookup_url_kwarg)
+        if code != None: 
+            room_Result = Room.objects.filter(code=code)
+            if len(room_Result) > 0:
+                room = room_Result[0]
+                self.request.session['room_code'] = code
+                return Response({'message': 'Room Joined!'}, status=status.HTTP_200_OK)
+            
+            return Response({'Bad Request': 'Invalid Room Code'}, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response({'Bad Request': 'Invalid post data, '}, status=status.HTTP_400_BAD_REQUEST)
+
+class CreateRoomView(APIView):
+    serializer_class = CreateRoomSerializer
+
+    def post(self, request, format=None):
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+
+        serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
             guest_can_pause = serializer.data.get('guest_can_pause')
             votes_to_skip = serializer.data.get('votes_to_skip')
@@ -33,9 +69,24 @@ class CreateRoomView(APIView):
                 room.guest_can_pause = guest_can_pause
                 room.votes_to_skip = votes_to_skip
                 room.save(update_fields=['guest_can_pause', 'votes_to_skip'])
+                self.request.session['room_code'] = room.code
+                return Response(RoomSerializer(room).data, status=status.HTTP_200_OK)
             else:
-                room = Room(host=host, guest_can_pause=guest_can_pause, votes_to_skip=votes_to_skip)
+                room = Room(host=host, guest_can_pause=guest_can_pause,
+                            votes_to_skip=votes_to_skip)
                 room.save()
-            
-            return Response(RoomSerializer(room).data, status=status.HTTP_201_CREATED)
-            
+                self.request.session['room_code'] = room.code
+                return Response(RoomSerializer(room).data, status=status.HTTP_201_CREATED)
+
+        return Response({'Bad Request': 'Invalid data...'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserInRoom(APIView):
+    def get(self, request,format=None):
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+
+        data = {
+            'code': self.request.session.get('room_code')
+        }
+        return JsonResponse(data, status=status.HTTP_200_OK)
--- music_controller/frontend/src/components/HomePage.js
+++ music_controller/frontend/src/components/HomePage.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import RoomJoinPage from "./RoomJoinPage";
 import CreateRoomPage from "./CreateRoomPage";
+import Room from "./Room";
+import { Grid, Button, ButonGroup, Typography, ButtonGroup } from "@material-ui/core";
+
 import {
   BrowserRouter as Router,
   Switch,
@@ -8,21 +11,66 @@ import {
   Link,
   Redirect,
 } from "react-router-dom";
+import { typography } from "@material-ui/system";
 
 export default class HomePage extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      roomCode: null,
+    };
+  }
+
+  async componentDidMount(){
+    fetch('/api/user-in-room')
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({
+          roomCode: data.code,
+        });
+      });
+  }
+
+  renderHomePage() {
+    return(
+      <Grid container spacing={3}>
+        <Grid item xs={12} align="center">
+          <Typography variant="h3" compact="h3">
+            House Party
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <ButtonGroup disableElevation variant="contained" color="primary">
+            <Button color="primary" to="/join" component={Link}>
+              Join a room
+            </Button>
+            <Button color="secondary" to="/create" component={Link}>
+              Create a Room
+            </Button>
+          </ButtonGroup>
+        </Grid>
+      </Grid>
+      
+    );
   }
 
   render() {
-    return (
+    return ( 
       <Router>
         <Switch>
-          <Route exact path="/">
-            <p>This is the home page</p>
+          <Route exact 
+          path="/" 
+          render={() =>{
+            return this.state.roomCode ? (
+              <Redirect to= {`/room/${this.state.roomCode}`} /> 
+            ) : (
+              this.renderHomePage()
+            );
+          }}>
           </Route>
           <Route path="/join" component={RoomJoinPage} />
           <Route path="/create" component={CreateRoomPage} />
+          <Route path="/room/:roomCode" component={Room} />
         </Switch>
       </Router>
     );
